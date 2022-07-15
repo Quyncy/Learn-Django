de70b5394c36d234dfa599344ad10544870825c4
Quynh <quynhmai@gmx.de>
2022-07-15 20:19:00 +0200

    40 add wait_for_db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,10 +1,28 @@
 """
 Django command fpr wait for database
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        pass
+        """ Entrypoint for command."""
+        self.stdout.write('Waiting for database')
+        # wir gehen davon aus das db = False ist bis wir es bestätigt bekommen
+        db_up = False
+        while db_up is False:
+            try:
+                # Wenn db nicht ready ist dann wird eine exception geworfen
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 sec...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available'))
--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -13,9 +13,8 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for database if database ready."""
+        """Test waiting for db if db ready"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -24,7 +23,7 @@ class CommandTests(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for database when getting OperationalError."""
+        """Test waiting for db when getting OperationalError"""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
