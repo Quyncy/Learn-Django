2ca15b21a37605b0588532015b7db1791828c98e
Quynh <quynhmai@gmx.de>
2022-07-17 15:59:51 +0200

    create user Model

--- app/core/models.py
+++ app/core/models.py
@@ -8,25 +8,37 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-
+# die Klasse UserManager bekommt BaseUserManager vererbt
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     # **extra_field  kann extra Argumente besorgen
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:  # leere email
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """"Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-# Abstractbaseuser ist für die authentifizierung
-# PermissionsMixin ist für die erlaubnis und Felder
+        return user
+
+
+# AbstractBaseUser ist für die authentifizierung
+# PermissionsMixin ist für die Erlaubnis und Felder
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     # unique macht die email adresse eindeutig
+    # hier wird die Datenbanktabelle User erstellt
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
