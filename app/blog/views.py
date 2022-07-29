b811d3625138fa10037fa2dfd678a1fc0c30d644
Quynh <quynhmai@gmx.de>
2022-07-29 12:38:23 +0200

    Database and migration

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Corey',
-        'title': 'Blog1',
-        'content': 'First post content',
-        'date': 'August 27,2018'
-    },
-    {
-        'author': 'Schafer',
-        'title': 'Blog2',
-        'content': 'Second post content',
-        'date': 'August 28,2018'
-    }
-]
 
 # a view always have to return a Httpresponse or a Exception
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
